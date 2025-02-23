ef5ec8d5587cc195e4e25c8fd0b9342e23769efe
abinesh-akr <12345sabinesh_bcs26@mepcoeng.ac.in>
2025-02-23 08:13:28 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Install Tesseract OCR manually
-echo "📌 Installing Tesseract OCR..."
-wget https://github.com/tesseract-ocr/tesseract/releases/download/5.3.2/tesseract-5.3.2-linux-x86_64.zip
+# ✅ Install Tesseract OCR & Poppler Manually
+echo "📌 Installing Tesseract OCR & Poppler..."
+wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.5.0.tar.gz
 unzip tesseract-5.3.2-linux-x86_64.zip -d tesseract
 export PATH=$PWD/tesseract:$PATH
 
-# Install Python dependencies
+wget https://github.com/oschwartz10612/poppler-windows/archive/refs/tags/v24.08.0-0.tar.gz
+tar -xf poppler-23.02.0.tar.xz
+export PATH=$PWD/poppler-23.02.0/bin:$PATH
+
+# ✅ Install Python Dependencies
 pip install -r requirements.txt
